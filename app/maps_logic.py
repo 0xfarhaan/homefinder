0b9e62e1278586599631ebb8dc6c1424242968cd
farhaan-ali <fa8g12@gmail.com>
2020-12-30 19:56:09 +0000

    Added postcodes to check transit time for in maps_logic

--- app/maps_logic.py
+++ app/maps_logic.py
@@ -1,10 +1,17 @@
 import googlemaps
 import os
 from datetime import datetime
+import csv
 
 API_KEY = os.getenv("API_KEY")
 gmaps = googlemaps.Client(key=API_KEY)
 
+csv_filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../postcodes.csv')
+
+postcodes = []  # postcodes for harringay
+with open(csv_filename, newline='') as inputfile:
+    for row in csv.reader(inputfile):
+        postcodes.append(row[0])
 
 def get_time_to_destination(start, end):
     """returns the transit time between two destinations using google maps api"""
