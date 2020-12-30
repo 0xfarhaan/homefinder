aa47fd6dd89dc44d34aa2d3cbf06675e133cdb03
farhaan-ali <fa8g12@gmail.com>
2020-12-30 12:46:37 +0000

    Added route to get travel time
    Added form to index page to get information

--- app/home_finder.py
+++ app/home_finder.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from maps_logic import get_time_to_destination
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/traveltime', methods=["POST"])
+def traveltime():
+    data = request.form
+    time = get_time_to_destination(data["start"], data["end"])
+    return time
 
 if __name__ == "__main__":
     app.run(debug=True)
