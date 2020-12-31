5f7fcd7daf8fc336c7bc240cb56b062f44264a2c
farhaan-ali <fa8g12@gmail.com>
2020-12-31 17:11:09 +0000

    added route for getting postcodes and updated template

--- app/home_finder.py
+++ app/home_finder.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for
-from maps_logic import get_time_to_destination
+from maps_logic import get_suitable_postcodes
+import json
 
 app = Flask(__name__)
 
@@ -7,10 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/api/traveltime', methods=["POST"])
-def traveltime():
-    time = get_time_to_destination(request.form["start"], request.form["end"])
-    return str(time)
+@app.route('/api/postcodes', methods=["POST"])
+def postcodes():
+    postcodes = ["IG12SZ", "N87EB"]  # Note this is a placeholder postcodes to check against
+    postcodes_list = get_suitable_postcodes(postcodes, request.form["office_postcode"], int(request.form["commute_time"]))
+    return json.dumps(postcodes_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
