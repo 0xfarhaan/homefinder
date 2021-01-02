d538d6b1f96f7773ce805a744285de0c690489ed
farhaan-ali <fa8g12@gmail.com>
2021-01-02 14:42:41 +0000

    Refactor new lines in flask app

--- app/home_finder.py
+++ app/home_finder.py
@@ -5,14 +5,17 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/postcodes/', methods=["POST"])
 def postcodes():
     postcodes_list = get_suitable_postcodes(load_postcodes(), request.form["office_postcode"], int(request.form["commute_time"]))
     return json.dumps(postcodes_list)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
